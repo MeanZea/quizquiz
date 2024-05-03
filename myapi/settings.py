e630e3833a60249a3ea9dcda8e658141c644d5d9
MeanZea <120470932+MeanZea@users.noreply.github.com>
2024-05-03 16:09:05 +0900

    Ready for deploy

--- myapi/settings.py
+++ myapi/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "whitenoise.middleware.WhiteNoiseMuddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "myapi.urls"
